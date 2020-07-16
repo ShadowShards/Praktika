a5762377265484f1a5914bede01cc12250fc710a
FireFlySS <mishafrend2015@yandex.ru>
2020-07-16 17:00:47 +0300

    Окончательная программа

--- pract/mainwindow.cpp
+++ pract/mainwindow.cpp
@@ -49,14 +49,23 @@ void MainWindow::on_pushButton_clicked()
 
         QTableWidgetItem *itmName = new QTableWidgetItem(this->personList[i].getName());
         QTableWidgetItem *itmSname = new QTableWidgetItem(this->personList[i].getSname());
+        QTableWidgetItem *itmPname = new QTableWidgetItem(this->personList[i].getPname());
         QTableWidgetItem *itmGender = new QTableWidgetItem(this->personList[i].getGender());
         QTableWidgetItem *itmAge = new QTableWidgetItem(QString::number(this->personList[i].getAge()));
-
+        QTableWidgetItem *itmProff = new QTableWidgetItem(this->personList[i].getProfession());
+        QTableWidgetItem *itmCiti = new QTableWidgetItem(this->personList[i].getCity());
+        QTableWidgetItem *itmStreet = new QTableWidgetItem(this->personList[i].getStreet());
+        QTableWidgetItem *itmIncome = new QTableWidgetItem(QString::number(this->personList[i].getIncome()));
 
         ui->tableWidget->setItem(i,0,itmName);
         ui->tableWidget->setItem(i,1,itmSname);
-        ui->tableWidget->setItem(i,2,itmGender);
-        ui->tableWidget->setItem(i,3,itmAge);
+        ui->tableWidget->setItem(i,2,itmPname);
+        ui->tableWidget->setItem(i,3,itmGender);
+        ui->tableWidget->setItem(i,4,itmAge);
+        ui->tableWidget->setItem(i,5,itmProff);
+        ui->tableWidget->setItem(i,6,itmIncome);
+        ui->tableWidget->setItem(i,7,itmCiti);
+        ui->tableWidget->setItem(i,8,itmStreet);
     }
 
 
